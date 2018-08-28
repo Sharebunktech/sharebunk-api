db2f146cee36ca0b837dddab554586f94aa817c6
Terrence <owusuterrence@gmail.com>
2018-08-28 05:01:14 +0100

    first commit

---
+++ server/models/player.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(Player) {
+
+    /**
+ * A search to the backend for music
+ * @param {string} artist User submits the kind of user to listen to his track
+ * @param {Function(Error, object)} callback
+ */
+
+    Player.search = function(artist, callback) {
+        // var artist;
+        // // TODO
+        // callback(null, artist);
+        console.log(artist)
+    };
+    
+};
